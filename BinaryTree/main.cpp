b4d939be5ef8d143e3dd16f4c063b16bc14bf52d
Chenyang <1427714873@qq.com>
2016-04-11 18:02:13 +0800

    ThreadingBinaryTree
    使用后序遍历构建线索二叉树

--- BinaryTree/main.cpp
+++ BinaryTree/main.cpp
@@ -9,8 +9,15 @@ struct BNode
     char data;
     BNode * lchild, * rchild;
     NodeTag ltag, rtag;
-    BNode():lchild(nullptr), rchild(nullptr) {}
-    BNode(char data):lchild(nullptr),rchild(nullptr) {this->data = data;}
+    BNode():lchild(nullptr), rchild(nullptr)
+    {
+        ltag = Link;
+        rtag = Link;
+    }
+    BNode(char data):BNode()
+    {
+        this->data = data;
+    }
 };
 
 /** \brief 根据输入的字符串生成一棵二叉树
@@ -57,21 +64,97 @@ void travel(BNode * root)
     travel(root->rchild);
 }
 
+BNode * pre = nullptr;
+void inThreading(BNode * root)
+{
+    if (root)
+    {
+        inThreading(root->lchild);
+        if (!root->lchild)
+        {
+            root->ltag = Thread;
+            root->lchild = pre;
+        }
+        if (!pre->rchild)
+        {
+            pre->rtag = Thread;
+            pre->rchild = root;
+        }
+        inThreading(root->rchild);
+    }
+}
 /** \brief 中序遍历二叉树root，并将其终须线索化
  *
  * \param root 二叉树的根节点root
  * \return 线索二叉树的根节点
  *
  */
-BNode * inOrderTheading(BNode * root)
+void inOrderTheading(BNode * root)
 {
     BNode * thrt = new BNode;//线索二叉树树根
     thrt->ltag = Link;
+    thrt->rtag = Thread;
+    thrt->rchild = thrt;
+    if (!root) thrt->lchild = thrt;
+    else
+    {
+        thrt->lchild = root;
+        pre = thrt;
+        inThreading(root);
+        pre->rchild = thrt;
+        pre->rtag = Thread;
+        thrt->rchild = pre;
+    }
+}
+
+BNode * search(BNode * root, char ch)
+{
+    if (root == nullptr) return nullptr;
+    if (root->data == ch) return root;
+    BNode * result = nullptr;
+    if (root->ltag == Link && (result = search(root->lchild, ch))) return result;
+    else if (root->rtag == Link && (result = search(root->rchild, ch))) return result;
+}
+
+void myInOrderTheading(BNode * root)
+{
+    if (root == nullptr) return;
+    myInOrderTheading(root->lchild);
+    myInOrderTheading(root->rchild);
+    BNode * p = nullptr;
+    for (p = root->lchild; p && p->rchild; p = p->rchild);
+    if (p)
+    {
+        p->rtag = Thread;
+        p->rchild = root;
+    }
+    for (p = root->rchild; p && p->lchild; p = p->lchild);
+    if (p)
+    {
+        p->ltag = Thread;
+        p->lchild = root;
+    }
 }
 
 int main()
 {
     BNode * root = createBiTree("-+a##*b##-c##d##/e##f##", 0);
-    travel(root);
-    destroy(root);
+    myInOrderTheading(root);
+    char ch;
+    while (cin >> ch && ch != '#')
+    {
+        BNode * result = search(root, ch);
+        if (result)
+        {
+            if (result->lchild)
+                cout << "左孩子" << result->lchild->data << endl;
+            else
+                cout << "nil" << endl;
+            if (result->rchild)
+                cout << "右孩子" << result->rchild->data << endl;
+            else
+                cout << "nil" << endl;
+        }
+
+    }
 }
